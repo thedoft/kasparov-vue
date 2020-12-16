092880c85450eb4b3e45d6f14f59d976d1d80cf4
thedoft <thedoft@gmail.com>
2020-12-16 22:07:52 +0300

    build to docs

--- src/main.js
+++ src/main.js
@@ -1,8 +1,6 @@
 import 'normalize.css';
-import Vue, { createApp } from 'vue';
+import { createApp } from 'vue';
 import App from './App.vue';
 import { router } from './router';
-
-Vue.config.productionTip = false;
 
 createApp(App).use(router).mount('#app');
